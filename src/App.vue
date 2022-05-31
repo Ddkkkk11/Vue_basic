8a34ccf2c189bd736d496fc31007557b96f897a6
xdd <45672942@qq.com>
2022-05-31 13:01:20 +0800

    Vue_cli

--- src/App.vue
+++ src/App.vue
@@ -1,131 +1,58 @@
 <template>
-  <div id="root">
-    <div class="todo-container">
-      <div class="todo-wrap">
-        <!--        头部-->
-        <Top :addTodo="addTodo"></Top>
-        <!--        列表项-->
-        <List :todoList='todoList' :changeValue="changeValue" :removeBtn="removeBtn"></List>
-        <!--        底部-->
-        <Bottom :todoList='todoList'
-                :clearEvery="clearEvery"
-                :hookAll="hookAll"
-        ></Bottom>
-      </div>
-    </div>
+  <div class="app">
+    <!--    <h1>我是app里面的组件学校的名字是{{ getName() }}</h1>-->
+    <!--    <h1 v-text="getName()"> 我是app里面的组件学校的名字是{{ getName() }}</h1>-->
+    <h1>{{ msg }}</h1>
+    <!--    通过父组件给子组件传递函数子组件把数据传给app -->
+    <School :getSChoolName="getSChoolName"></School>
+    <hr>
+    <!--    通过父组件给子组件绑定一个自定义事件实现：子数据给父传递(第一种写法,使用@或v-on)-->
+    <!--    <Student v-on:xdd="getStudentName"></Student>-->
+    <Student v-on:xdd.once="getStudentName"></Student>
+    <!--    通过父组件给子组件绑定一个自定义事件实现：子数据给父传递(第一种写法,使用ref this.$refs.student.$on('xdd',getStudentName)-->
+    <!--    <Student ref="student"></Student>-->
   </div>
 </template>
 <script>
-//引入组件
-import Top from "@/components/Top";
-import Bottom from "@/components/Bottom";
-import List from "@/components/list";
+//引入组件School
+import School from "./components/School.vue";
+import Student from "./components/Student.vue";
 
 export default {
   name: "App",
   components: {
-    Top, Bottom, List,
+    School,
+    Student,
   },
   data() {
     return {
-      todoList: [
-        {id: '001', title: '吃饭', done: false},
-        {id: '002', title: '睡觉', done: false},
-        {id: '003', title: 'code', done: true},
-        {id: '004', title: 'English', done: false},
-      ],
+      msg: '你好'
     }
   },
   methods: {
-    //添加信息
-    addTodo(x) {
-      // console.log("我收到了数据" + x);
-      this.todoList.unshift(x);
+    getSChoolName(name) {
+      console.log("app收到了学校名称是:", name);
+      return name;
     },
-    //勾选or取消勾选
-    changeValue(id) {
-      this.todoList.forEach((todo) => {
-        // console.log(todo)
-        if (todo.id === id) todo.done = !todo.done;
-      })
-    },
-    //删除
-    removeBtn(id) {
-      /* this.todoList.forEach((Ele, index) => {
-         if (Ele.id === id) {
-           this.todoList.splice(index, 1);
-         }
-       });*/
-      this.todoList = this.todoList.filter((todo) => {
-        return todo.id !== id;
-      })
-    },
-    //删除全部
-    clearEvery() {
-      this.todoList = this.todoList.filter((todo) => {
-        // if (todo.id === id)
-        return !todo.done
-      })
-    },
-    //全选
-    hookAll(judge) {
-      this.todoList.forEach((value) => value.done = judge)
+    getStudentName(name, ...params) {
+      console.log("app收到了学生名称是 ",);
+      console.log(...params);
     }
-
+  },
+  mounted() {
+    //使用ref 可以延时在绑定自定义事件
+    setTimeout(() => {
+      // this.$refs.student.$on('xdd', this.getStudentName)//绑定自定义事件
+      //只能绑定一次
+      // console.log(this.$refs.student.gender)
+      this.$refs.student.$once('xdd', this.getStudentName);//绑定事件只发生一次
+    }, 0);
   }
 };
 </script>
 <style>
-/*base*/
-body {
-  background: #fff;
+.app {
+  background-color: #cccccc;
+  padding: 5px;
 }
-
-.btn {
-  display: inline-block;
-  padding: 4px 12px;
-  margin-bottom: 0;
-  font-size: 14px;
-  line-height: 20px;
-  text-align: center;
-  vertical-align: middle;
-  cursor: pointer;
-  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
-  border-radius: 4px;
-}
-
-.btn-danger {
-  color: #fff;
-  background-color: #da4f49;
-  border: 1px solid #bd362f;
-}
-
-.btn-danger:hover {
-  color: #fff;
-  background-color: #bd362f;
-}
-
-.btn:focus {
-  outline: none;
-}
-
-.todo-container {
-  width: 600px;
-  margin: 0 auto;
-}
-
-.todo-container .todo-wrap {
-  padding: 10px;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-}
-
-
-/*main*/
-
-/*item*/
-
-/*footer*/
-
-
 </style>
---
+++ src/components/Student.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="student">
+    <h2>学生名称:{{ name }}</h2>
+    <h2>学生性别:{{ gender }}</h2>
+    <button @click="sendSchoolName">点我把学生名称给app</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Student",
+  data() {
+    return {
+      name: 'xdd',
+      gender: '男',
+      num: 10
+    };
+  },
+  methods: {
+    sendSchoolName() {
+      //触发Student组件实例身上的xdd 事件
+          this.$emit('xdd', this.name,123,123,12,'qweasd','aasd','a','dasd','ad','asd','a','d','asd','ad',123)
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+.student {
+  background-color: brown;
+  padding: 5px;
+
+  h2 {
+    color: #42b983;
+    font-size: 50px;
+  }
+}
+
+</style>
