5bb6e4f0162e7532d13433a8bee5269c3d2f347e
xdd <45672942@qq.com>
2022-07-13 22:53:00 +0800

    Vue_cli

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import Vue from 'vue'
 import App from './App.vue'
 //关闭Vue的生产提示
 Vue.config.productionTip = false
-
+//引入插件
+import vueResource from 'vue-resource'
+//使用插件
+Vue.use(vueResource);
 //创建vm
 new Vue({
-    el: '#app',
-    render: h => h(App),
-    beforeCreate() {
-        Vue.prototype.$bus = this
-    }
+  el: '#app',
+  render: h => h(App),
+  beforeCreate() {
+    Vue.prototype.$bus = this
+  }
 })
--- vue.config.js
+++ vue.config.js
@@ -11,5 +11,31 @@ module.exports = {
       entry: 'src/main.js',
     },
   },
-  lintOnSave:false //关闭语法检查
+  lintOnSave: false, //关闭语法检查
+  //开启代理服务器 方式1
+  /*  devServer: {
+       proxy: 'http://localhost:5000'
+   }, */
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        pathRewrite: {
+          '^/api': ''
+        },
+        ws: true, //用于支持websocket
+        changeOrigin: true //用于控制请求头中的host的值
+      },
+      '/demo': {
+        target: 'http://localhost:5001',
+        pathRewrite: {
+          '^/demo': ''
+        },
+        ws: true, //用于支持websocket
+        changeOrigin: true //用于控制请求头中的host的值
+      },
+
+
+    }
+  }
 }
